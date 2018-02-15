9f130ad7a79dd75c932e7c7dad57f0860a991d9e
cellay <camposellay@gmail.com>
2018-02-15 11:28:04 -0500

    CAmbiando función click

--- js/app.js
+++ js/app.js
@@ -8,27 +8,37 @@ let success = ({ coords }) => {
 
   let getInfo = (data) => {
     // página principal
-    $('.box').html(`<h1>${data.currently.temperature}°F</h1>
+    main(); 
+    function main() {
+      $('.box').html('');
+      $('.box').html(`<h1>${data.currently.temperature}°F</h1>
       <p><b>Wind: </b>${data.currently.windSpeed}m/s</p>
       <p><b>Humidity: </b>${data.currently.humidity}</p>
       <p><b>UV Index: </b>${data.currently.uvIndex}</p>
       <p><b>Pressure: </b>${data.currently.pressure}</p>
       <a id="week" href="#">Week prediction</a>
       `);
+      
+      $('#week').click(() => {
+        console.log(data.daily.data[0]);
+        $('.box').html('');
+        $('.box').html(`<h1>Prediction</h1>
+        <p><b>Tomorrow: </b>${data.daily.data[0].apparentTemperatureMin}°F - ${data.daily.data[0].apparentTemperatureMax}°F</p>
+        <a id="back" href="#">Back</a>
+        `);
+  
+        $('#back').click(() => {
+          main();
+        });
+      }); 
+    };
 
     // background with unsplash
     let url = 'https://source.unsplash.com/800x600/?';
     $('body').css({ "background": "url('" + url + data.currently.icon + "')", "background-repeat": "no-repeat", "background-size": "cover" });
 
     // Event for prediction
-    $('#week').click(() => {
-      console.log(data.daily.data[0]);
-      $('.box').html('');
-      $('.box').html(`<h1>Prediction</h1>
-      <p><b>Tomorrow: </b>${data.daily.data[0].apparentTemperatureMin}°F - ${data.daily.data[0].apparentTemperatureMax}°F</p>
-      <a id="back" href="#">Back</a>
-      `);
-    });
+       
   };
 
   $.ajax({
