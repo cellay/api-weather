b6732d29eacefc352be2eb0554c894346c378cb5
cellay <camposellay@gmail.com>
2018-02-15 10:58:22 -0500

    Añadiendo eslint

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const KEY = '034114a19341cc0d8cead3296c91ba28/';
 
-  let success = ({ coords }) => {
-    let myPosition = coords.latitude + ',' + coords.longitude;
-    let URI = `https://api.darksky.net/forecast/`;
+let success = ({ coords }) => {
+  let myPosition = coords.latitude + ',' + coords.longitude;
+  let URI = `https://api.darksky.net/forecast/`;
 
-    let getInfo = (data) => {
-      // página principal
-      $('.box').html(`<h1>${data.currently.temperature}°F</h1>
+  let getInfo = (data) => {
+    // página principal
+    $('.box').html(`<h1>${data.currently.temperature}°F</h1>
       <p><b>Wind: </b>${data.currently.windSpeed}m/s</p>
       <p><b>Humidity: </b>${data.currently.humidity}</p>
       <p><b>UV Index: </b>${data.currently.uvIndex}</p>
@@ -16,27 +16,27 @@ const KEY = '034114a19341cc0d8cead3296c91ba28/';
       <a id="week" href="#">Week prediction</a>
       `);
 
-      // background with unsplash
-      let url = 'https://source.unsplash.com/800x600/?';
-      $('body').css({"background": "url('"+ url + data.currently.icon + "')", "background-repeat": "no-repeat", "background-size": "cover"});
+    // background with unsplash
+    let url = 'https://source.unsplash.com/800x600/?';
+    $('body').css({ "background": "url('" + url + data.currently.icon + "')", "background-repeat": "no-repeat", "background-size": "cover" });
 
-      // Event for prediction
-      $('#week').click(() => {
-        console.log(data.daily.data[0]);
-        $('.box').html('');
-        $('.box').html(`<h1>Prediction</h1>
+    // Event for prediction
+    $('#week').click(() => {
+      console.log(data.daily.data[0]);
+      $('.box').html('');
+      $('.box').html(`<h1>Prediction</h1>
       <p><b>Tomorrow: </b>${data.daily.data[0].apparentTemperatureMin}°F - ${data.daily.data[0].apparentTemperatureMax}°F</p>
       <a id="back" href="#">Back</a>
       `);
-      })
-    }
+    });
+  };
 
-    $.ajax({
-      url: proxy + URI + KEY + myPosition,
-      success: getInfo
-    })
-  }
+  $.ajax({
+    url: proxy + URI + KEY + myPosition,
+    success: getInfo
+  })
+}
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success);
-  }
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success);
+}
